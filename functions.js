08eed9db90745b7b0414179ffe897b0070f75523
Gopalakrishna-Kondabolu <kondabolugopalakrishna2005@gmail.com>
2025-02-07 01:52:25 -0800

    gopal

---
+++ functions.js
@@ -0,0 +1,12 @@
+// function declarations
+function greet1(name){
+    return `hello,${name}!`;
+}
+console.log(greet1("gopal"));
+
+// function expressions
+
+const greet2 = function(name){
+    return `hello,${name}`;
+}
+console.log(greet2("laxman")); 
