24bfbfac03da855d25d88083bf8fffe4299f51cf
Gopalakrishna Kondabolu <kondabolugopalakrishna2005@gmail.com>
2025-02-19 13:34:00 +0530

    Add files via upload

---
+++ Forward.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Forward.css";
+
+export const Forward = () => {
+    return (
+            <div className="Forward-main">
+                <div className="Forward-content">
+                    <div className="Forward-title">
+                        Welcome to Stanjo <br /> -Lighting the Way <br /> Forward
+                    </div>
+                    <div className="Forward-matter">
+                        At Stanjo, we believe lighting is more than just <br /> illumination—it's about enhancing spaces, creating <br /> ambiance, and delivering unmatched efficiency. <br /> Our mission is to provide high-quality, energy-efficient <br /> LED lighting solutions that blend innovation, <br /> durability, and style to brighten homes, offices, and <br /> outdoor spaces.
+                    </div>
+                    <div className="Forward-button"> Explore Products</div>
+                </div>
+            </div>
+    );
+};
